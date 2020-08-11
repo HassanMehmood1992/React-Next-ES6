ab01196a76fb3e11c8d83f8b7a3a045c7ffec5b7
Hassan Mehmood <hmehmood92@gmail.com>
2020-08-11 23:59:09 +0400

    initial commit

---
+++ pages/main/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component<{}> {
+    render() {
+        return (
+            <div className="test">
+                <div className="main">ssd</div>
+            </div>
+        );
+    }
+}
+
+export default Main;
